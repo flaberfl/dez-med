4e2ff4e65d31dc73856c617c024ebc5ccc1774b5
Андрей Олейник <48767673+flaberfl@users.noreply.github.com>
2023-03-17 23:04:11 +0300

    home

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -110,6 +110,67 @@ function initSliders() {
 			}
 		});
 	}
+	if (document.querySelector('.docs__slider')) { // Указываем класс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.docs__slider', {
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation],
+			/*
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 60,
+			// autoHeight: true,
+			speed: 800,
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+			// Dotts
+			// pagination: {
+			// 	el: '.swiper-pagination-types',
+			// 	clickable: true,
+			// },
+			// Arrows
+			navigation: {
+				nextEl: '.swiper-types-btn_next',
+				prevEl: '.swiper-types-btn_prev',
+			},
+
+			// breakpoints: {
+			// 	320: {
+			// 		slidesPerView: 1,
+			// 		spaceBetween: 0,
+			// 	},
+			// 	768: {
+			// 		slidesPerView: 2,
+			// 		spaceBetween: 30,
+			// 	},
+			// 	992: {
+			// 		enabled: true,
+			// 		slidesPerView: 3,
+			// 		spaceBetween: 60,
+			// 	},
+			// 	// 1268: {
+			// 	// 	slidesPerView: 4,
+			// 	// 	spaceBetween: 30,
+			// 	// },
+			// },
+
+			on: {
+
+			}
+		});
+	}
 	// if (document.querySelector('.swiper__sets')) { // Указываем класс нужного слайдера
 	// 	// Создаем слайдер
 	// 	new Swiper('.swiper__sets-slider', {
